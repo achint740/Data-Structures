2e637504fecc28cb5fbf5b44086cf8652f3135a8
Achint <42889869+achint740@users.noreply.github.com>
2021-10-01 14:59:15 +0530

    Update Height.cpp

--- Trees/Height.cpp
+++ Trees/Height.cpp
@@ -11,7 +11,10 @@ class Node {
     int height(Node* root) {
         if(root==NULL)
           return -1;
-        
-        return 1+max(height(root->left),height(root->right));
+        int left = height(root->left);
+        int right = height(root->right);
+        int ans = max(left, right) + 1;
+        return ans;
+//         return 1+max(height(root->left),height(root->right));
         // Write your code here.
     }
